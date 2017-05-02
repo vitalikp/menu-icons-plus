257f0998606a8fcb28760e2960bf316b26d2e425
Perevertun V.M. <vitalik.perevertun@gmail.com>
2017-05-02 23:32:26 +0300

    * about: show extension homepage url from install.rdf file;

--- content/about.js
+++ content/about.js
@@ -10,5 +10,18 @@ function init()
 	var extensionVersion = document.getElementById("extensionVersion");
 	extensionVersion.setAttribute("value", extensionsStrings.getFormattedString("aboutVersion", [addon.version]));
 
+	var extensionHomepage = document.getElementById("extensionHomepage");
+	extensionHomepage.setAttribute("value", addon.name);
+	extensionHomepage.setAttribute("url", addon.homepageURL);
+
 	sizeToContent();
+}
+
+function openHomePage(aEvent)
+{
+	if (aEvent.button != 0)
+		return;
+
+	window.close();
+	openUILinkIn(aEvent.target.getAttribute("url"), 'tab');
 }
