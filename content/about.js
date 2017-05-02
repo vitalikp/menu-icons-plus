7da01756f5acc3442d08c618c4efd7b15a6859a1
Perevertun V.M. <vitalik.perevertun@gmail.com>
2017-05-02 20:36:35 +0300

    * about: show extension version from install.rdf file;

--- content/about.js
+++ content/about.js
@@ -2,9 +2,13 @@
 function init()
 {
 	var addon = window.arguments[0];
+	var extensionsStrings = document.getElementById("extensionsStrings");
 
 	var extensionName = document.getElementById("extensionName");
 	extensionName.textContent = addon.name;
+
+	var extensionVersion = document.getElementById("extensionVersion");
+	extensionVersion.setAttribute("value", extensionsStrings.getFormattedString("aboutVersion", [addon.version]));
 
 	sizeToContent();
 }
