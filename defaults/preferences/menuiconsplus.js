eb3fac3ce0e1e5956b38a4dd528a0df5ba040818
Perevertun V.M. <vitalik.perevertun@gmail.com>
2017-05-20 15:22:42 +0300

    * install.rdf: move contributors to about properties file;

--- defaults/preferences/menuiconsplus.js
+++ defaults/preferences/menuiconsplus.js
@@ -3,3 +3,8 @@ pref('extensions.menuiconsplus.hidedisabledmenuicons', false);
 pref('extensions.menuiconsplus.usethememenuicons', false);
 pref('extensions.menuiconsplus.icongridstylesheet', 'chrome://menuiconsplus/skin/Fugue.css');
 pref('extensions.menuiconsplus@vitalikp.description', 'chrome://menuiconsplus/locale/menuiconsplus.properties');
+pref('extensions.menuiconsplus@vitalikp.contributor.1', 'chrome://menuiconsplus/locale/about.properties');
+pref('extensions.menuiconsplus@vitalikp.contributor.2', 'chrome://menuiconsplus/locale/about.properties');
+pref('extensions.menuiconsplus@vitalikp.contributor.3', 'chrome://menuiconsplus/locale/about.properties');
+pref('extensions.menuiconsplus@vitalikp.contributor.4', 'chrome://menuiconsplus/locale/about.properties');
+pref('extensions.menuiconsplus@vitalikp.contributor.5', 'chrome://menuiconsplus/locale/about.properties');
