b3e09e8785f219e2db064588b3c3ff72e8dd8708
Perevertun V.M. <vitalik.perevertun@gmail.com>
2018-03-21 13:51:06 +0200

    * overlay.js: use tab character instead of spaces for indent;

--- content/overlay.js
+++ content/overlay.js
@@ -39,206 +39,206 @@ const Cc = Components.classes;
 
 var menuIconsPlus =
 {
-  onLoad: function()
-  {
-    // initialization code
-    this.debug = true;
-    this.prefs = Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefService).getBranch('extensions.menuiconsplus.');
+	onLoad: function()
+	{
+		// initialization code
+		this.debug = true;
+		this.prefs = Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefService).getBranch('extensions.menuiconsplus.');
 
-    // load bindings
-    this.loadStyleSheet('chrome://menuiconsplus/skin/bindings.css');
+		// load bindings
+		this.loadStyleSheet('chrome://menuiconsplus/skin/bindings.css');
 
-    // non-Windows OS
-    var nonWindows = (navigator.platform.indexOf('Win') == -1);
-    if (nonWindows)
-      this.loadStyleSheet('chrome://menuiconsplus/skin/non_windows.css');
+		// non-Windows OS
+		var nonWindows = (navigator.platform.indexOf('Win') == -1);
+		if (nonWindows)
+			this.loadStyleSheet('chrome://menuiconsplus/skin/non_windows.css');
 
-    /* load rest of stylesheets depending on user prefs */
-    // menu icon set
-    this.loadStyleSheet(this.prefs.getCharPref('icongridstylesheet'));
+		/* load rest of stylesheets depending on user prefs */
+		// menu icon set
+		this.loadStyleSheet(this.prefs.getCharPref('icongridstylesheet'));
 
-    // external icons
-    if (this.prefs.getCharPref('icongridstylesheet') || this.prefs.getBoolPref('usethememenuicons'))
-      this.loadStyleSheet('chrome://menuiconsplus/skin/external_icons.css');
+		// external icons
+		if (this.prefs.getCharPref('icongridstylesheet') || this.prefs.getBoolPref('usethememenuicons'))
+			this.loadStyleSheet('chrome://menuiconsplus/skin/external_icons.css');
 
-    // checkmark and radio button style
-    switch (this.prefs.getIntPref('checkmarkstyle'))
-    {
-      case 0:
-        this.loadStyleSheet('chrome://menuiconsplus/skin/os_checkmarks_radiobuttons.css');
-        break;
+		// checkmark and radio button style
+		switch (this.prefs.getIntPref('checkmarkstyle'))
+		{
+			case 0:
+				this.loadStyleSheet('chrome://menuiconsplus/skin/os_checkmarks_radiobuttons.css');
+				break;
 
-      case 1:
-        this.loadStyleSheet('chrome://menuiconsplus/skin/alt_checkmarks_radiobuttons.css');
-        break;
+			case 1:
+				this.loadStyleSheet('chrome://menuiconsplus/skin/alt_checkmarks_radiobuttons.css');
+				break;
 
-      case 2:
-        this.loadStyleSheet('chrome://menuiconsplus/skin/iconset_checkmarks_radiobuttons.css');
-        break;
+			case 2:
+				this.loadStyleSheet('chrome://menuiconsplus/skin/iconset_checkmarks_radiobuttons.css');
+				break;
 
-      default:
-    }
+			default:
+		}
 
-    // show colored icons
-    if (this.prefs.getBoolPref('showcoloredicons'))
-    	this.loadStyleSheet('chrome://menuiconsplus/skin/color-icons.css');
+	// show colored icons
+	if (this.prefs.getBoolPref('showcoloredicons'))
+		this.loadStyleSheet('chrome://menuiconsplus/skin/color-icons.css');
 
-    // show identity icons
-    if (this.prefs.getBoolPref('showidentityicons'))
-    	this.loadStyleSheet('chrome://menuiconsplus/skin/identity.css');
+	// show identity icons
+	if (this.prefs.getBoolPref('showidentityicons'))
+		this.loadStyleSheet('chrome://menuiconsplus/skin/identity.css');
 
-    // use theme icons
-    if (this.prefs.getBoolPref('usethememenuicons') && !nonWindows)
-    {
-      this.loadStyleSheet('chrome://menuiconsplus/skin/browser_theme_icons.css');
+	// use theme icons
+	if (this.prefs.getBoolPref('usethememenuicons') && !nonWindows)
+	{
+		this.loadStyleSheet('chrome://menuiconsplus/skin/browser_theme_icons.css');
 
-      // use icons from classic theme restorer if available
-      var key = document.getElementById('ctraddon_key_toggleCtrAddonBar');
-      if (key)
-        this.loadStyleSheet('chrome://menuiconsplus/skin/ctr_icons.css');
-    }
+		// use icons from classic theme restorer if available
+		var key = document.getElementById('ctraddon_key_toggleCtrAddonBar');
+		if (key)
+			this.loadStyleSheet('chrome://menuiconsplus/skin/ctr_icons.css');
+	}
 
-    if (this.prefs.getBoolPref('usegtkicons'))
-    	this.loadStyleSheet('chrome://menuiconsplus/skin/gtkstockicons.css');
+	if (this.prefs.getBoolPref('usegtkicons'))
+		this.loadStyleSheet('chrome://menuiconsplus/skin/gtkstockicons.css');
 
-    // disabled menuitem icons
-    if (this.prefs.getBoolPref('hidedisabledmenuicons'))
-      this.loadStyleSheet('chrome://menuiconsplus/skin/hide_disabled.css');
-    else
-      this.loadStyleSheet('chrome://menuiconsplus/skin/default_disabled.css');
+	// disabled menuitem icons
+	if (this.prefs.getBoolPref('hidedisabledmenuicons'))
+		this.loadStyleSheet('chrome://menuiconsplus/skin/hide_disabled.css');
+	else
+		this.loadStyleSheet('chrome://menuiconsplus/skin/default_disabled.css');
 
-    // take care of 'image' attributes
-    if (this.prefs.getCharPref('icongridstylesheet'))
-    {
-      // webdeveloper
-      this.removeAttrib([document.getElementById('web-developer-menu'), document.getElementById('web-developer-context')], 'image');
+	// take care of 'image' attributes
+	if (this.prefs.getCharPref('icongridstylesheet'))
+	{
+		// webdeveloper
+		this.removeAttrib([document.getElementById('web-developer-menu'), document.getElementById('web-developer-context')], 'image');
 
-      var menu = document.getElementById('web-developer-menu');
-      if (menu)
-      {
-        this.loadStyleSheet('chrome://menuiconsplus/skin/webdeveloper.css');
-        var submenus = menu.getElementsByTagName('menu');
-        this.removeAttrib(submenus, 'image');
-      }
+		var menu = document.getElementById('web-developer-menu');
+		if (menu)
+		{
+			this.loadStyleSheet('chrome://menuiconsplus/skin/webdeveloper.css');
+			var submenus = menu.getElementsByTagName('menu');
+			this.removeAttrib(submenus, 'image');
+		}
 
-      menu = document.getElementById('web-developer-context');
-      if (menu)
-      {
-        submenus = menu.getElementsByTagName('menu');
-        this.removeAttrib(submenus, 'image');
-      }
+		menu = document.getElementById('web-developer-context');
+		if (menu)
+		{
+			submenus = menu.getElementsByTagName('menu');
+			this.removeAttrib(submenus, 'image');
+		}
 
-      // gspace
-      var panel = document.getElementById('gspace-panel');
-      if (panel)
-      {
-        var buttons = panel.getElementsByTagName('toolbarbutton');
-        this.removeAttrib(buttons, 'image');
-      }
+		// gspace
+		var panel = document.getElementById('gspace-panel');
+		if (panel)
+		{
+			var buttons = panel.getElementsByTagName('toolbarbutton');
+			this.removeAttrib(buttons, 'image');
+		}
 
-      // mystickies
-      this.removeAttrib([document.getElementById('context-mystickies-addNote')], 'image');
-      this.removeAttrib([document.getElementById('menu-mystickies-addNote')], 'image');
-      var toolbar = document.getElementById('toolbar-mystickies');
-      if (toolbar)
-      {
-        var buttons = toolbar.getElementsByTagName('toolbarbutton');
-        this.removeAttrib(buttons, 'image');
-      }
+		// mystickies
+		this.removeAttrib([document.getElementById('context-mystickies-addNote')], 'image');
+		this.removeAttrib([document.getElementById('menu-mystickies-addNote')], 'image');
+		var toolbar = document.getElementById('toolbar-mystickies');
+		if (toolbar)
+		{
+			var buttons = toolbar.getElementsByTagName('toolbarbutton');
+			this.removeAttrib(buttons, 'image');
+		}
 
-      // similarweb
-      var popup = document.getElementById('menuStatusbarOptions');
-      if (popup)
-      {
-        var mitems = popup.getElementsByTagName('menuitem');
-        this.removeAttrib(mitems, 'image');
-      }
+		// similarweb
+		var popup = document.getElementById('menuStatusbarOptions');
+		if (popup)
+		{
+			var mitems = popup.getElementsByTagName('menuitem');
+			this.removeAttrib(mitems, 'image');
+		}
 
-      var btn = document.getElementById('similarweb-options-button');
-      if (btn)
-      {
-        var mitems = btn.firstChild.childNodes;
-        this.removeAttrib(mitems, 'image');
-      }
+		var btn = document.getElementById('similarweb-options-button');
+		if (btn)
+		{
+			var mitems = btn.firstChild.childNodes;
+			this.removeAttrib(mitems, 'image');
+		}
 
-      // tamper data icon redux
-      this.removeAttrib([document.getElementById('appmenu-webDeveloper-tdiconmenu')], 'image');
-      this.removeAttrib([document.getElementById('menu_webDeveloper_tdiconmenu')], 'image');
-    }
+		// tamper data icon redux
+		this.removeAttrib([document.getElementById('appmenu-webDeveloper-tdiconmenu')], 'image');
+		this.removeAttrib([document.getElementById('menu_webDeveloper_tdiconmenu')], 'image');
+	}
 
-    this.initialized = true;
-  },
+	this.initialized = true;
+},
 
-  loadStyleSheet: function(aChromeURI)
-  {
-    // check param
-    if (typeof aChromeURI != 'string'
-      || (aChromeURI.slice(0, 9) != 'chrome://' && aChromeURI.slice(0, 8) != 'file:///')) // restricted to local URI's, just to be safe
-        return false;
+	loadStyleSheet: function(aChromeURI)
+	{
+		// check param
+		if (typeof aChromeURI != 'string'
+			|| (aChromeURI.slice(0, 9) != 'chrome://' && aChromeURI.slice(0, 8) != 'file:///')) // restricted to local URI's, just to be safe
+			return false;
 
-    // set up stylesheet service
-    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-    var ios = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
+		// set up stylesheet service
+		var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
+		var ios = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
 
-    try
-    {
-      var uri = ios.newURI(aChromeURI, null, null);
-      if (!sss.sheetRegistered(uri, sss.AGENT_SHEET))
-        sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
-    }
-    catch(e)
-    {
-        Components.utils.reportError(e); // report the error
-        return false;
-    }
+		try
+		{
+			var uri = ios.newURI(aChromeURI, null, null);
+			if (!sss.sheetRegistered(uri, sss.AGENT_SHEET))
+				sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+		}
+		catch(e)
+		{
+			Components.utils.reportError(e); // report the error
+			return false;
+		}
 
-    return true;
-  },
+		return true;
+	},
 
-  removeAttrib: function(aElementArray, aAttribute)
-  {
-    for (var i = 0, j = aElementArray.length; i < j; i++)
-    {
-      if (!aElementArray[i]) // if the element doesn't exist, skip it
-        continue;
+	removeAttrib: function(aElementArray, aAttribute)
+	{
+		for (var i = 0, j = aElementArray.length; i < j; i++)
+		{
+			if (!aElementArray[i]) // if the element doesn't exist, skip it
+				continue;
+	
+			if (aElementArray[i].hasAttribute(aAttribute))
+				aElementArray[i].removeAttribute(aAttribute);
+		}
+	},
 
-      if (aElementArray[i].hasAttribute(aAttribute))
-        aElementArray[i].removeAttribute(aAttribute);
-    }
-  },
+	miniMapFix: function()
+	{
+		var btn = document.getElementById('addAddressPanel'),
+			bClass = btn.className;
 
-  miniMapFix: function()
-  {
-    var btn = document.getElementById('addAddressPanel'),
-        bClass = btn.className;
+		if (btn.getAttribute('image') == 'chrome://minimap/skin/accept.png')
+		{
+			btn.className += ' checked';
+			btn.setAttribute('image', '');
+		}
+		else
+			if (!btn.hasAttribute('image'))
+			{
+				if (bClass.indexOf(' checked') != -1)
+					btn.className = bClass.replace(' checked', '');
+			}
+	},
 
-    if (btn.getAttribute('image') == 'chrome://minimap/skin/accept.png')
-    {
-      btn.className += ' checked';
-      btn.setAttribute('image', '');
-    }
-    else
-      if (!btn.hasAttribute('image'))
-      {
-        if (bClass.indexOf(' checked') != -1)
-          btn.className = bClass.replace(' checked', '');
-      }
-  },
+	webDevFix: function(aSuffix)
+	{
+		var mitem = document.getElementById('web-developer-dom-inspector-' + aSuffix);
+		if (mitem && !mitem.className)
+			mitem.className = 'menuitem-iconic';
+	},
 
-  webDevFix: function(aSuffix)
-  {
-    var mitem = document.getElementById('web-developer-dom-inspector-' + aSuffix);
-    if (mitem && !mitem.className)
-      mitem.className = 'menuitem-iconic';
-  },
-
-  log: function(msg)
-  {
-    if (this.debug) 
-    {
-      window.dump('MenuIconsPlus: ' + msg + '\n');
-    }
-  }
+	log: function(msg)
+	{
+		if (this.debug) 
+		{
+			window.dump('MenuIconsPlus: ' + msg + '\n');
+		}
+	}
 };
 
 window.addEventListener('load', function(e) { menuIconsPlus.onLoad(e); }, false);
